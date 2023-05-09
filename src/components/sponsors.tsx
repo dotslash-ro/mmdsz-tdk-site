17454973bb87b7fdf62f0324c1a6a9cae81866b4
godraadam <godra.adam@gmail.com>
2023-05-09 15:25:11 +0300

    sponsors

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -11,10 +11,54 @@ import gordonLogo from "../assets/gordon-logo.jpg";
 import univerLogo from "../assets/univer-logo.png";
 import gym18Logo from "../assets/18gym-logo.png";
 import cafe112Logo from "../assets/cafe112-logo2.png";
-
+import pavaLogo from "../assets/pava-logo.png";
+import hcLogo from "../assets/hospital-logo.jpg";
+import toyotaLogo from "../assets/toyota-logo2.png";
+import clinilabLogo from "../assets/clinilab-logo.png";
+import osteoLogo from "../assets/osteopharm-logo.png";
+import biaLogo from "../assets/bia-logo.png";
+import crLogo from "../assets/cr-logo.png";
+import storzLogo from "../assets/storz-logo.jpg";
+import cardiomedLogo from "../assets/cardiomed-logo.png";
+import bioeelLogo from "../assets/bioeel-logo.jpg";
+import fomcoLogo from "../assets/fomco-logo.jpg";
+import bsbLogo from "../assets/bsb-logo.png";
+import medicinaLogo from "../assets/medicina-logo.png";
+import angioLogo from "../assets/angio-logo.jpg";
+import tablourLogo from "../assets/tablour-logo.png";
+import aomLogo from "../assets/aom-logo.jpg";
+import marmedLogo from "../assets/marmed-logo.png";
+import jockerLogo from "../assets/jocker-logo.png";
+import darociLogo from "../assets/daroci-logo.png";
+import tobozLogo from "../assets/toboz-logo.png";
+import hookahLogo from "../assets/hookah-logo.png";
+import bertisLogo from "../assets/bertis-logo.jpg";
+import dcLogo from "../assets/dc-logo.png";
+import vitalityLogo from "../assets/vitality-logo.png";
+import invitedorLogo from "../assets/invitedor-logo.png";
+import lcLogo from "../assets/lc-logo.png";
+import naturaliLogo from "../assets/naturali-logo.jpeg";
+import daliaLogo from "../assets/dalia-logo.png";
+import tataiLogo from "../assets/tatai-logo.png";
+import labventLogo from "../assets/labvent-logo.png";
+import ikLogo from "../assets/ik-logo.jpg";
+import ponpaonLogo from "../assets/ponpaon-logo.jpg";
+import pontisLogo from "../assets/pontis-logo.png";
+import mmtLogo from "../assets/mmt-logo.jpg";
+import npaLogo from "../assets/npa.jpeg";
+import cmrLogo from "../assets/cmr-logo.jpeg";
+import cfrLogo from "../assets/cfr-logo.jpeg";
+import gndLogo from "../assets/gnd-logo.png";
+import ginemedLogo from "../assets/ginemed-logo.jpg";
+import zpLogo from "../assets/zp-logo.png";
+import beatLogo from "../assets/beat-logo.png";
+import countryLogo from "../assets/country-logo.jpg";
+import dscLogo from "../assets/dsc-logo.png";
+import dwLogo from "../assets/dw-logo.png";
 import sgpLogo from "../assets/shadow-gastro-pub.webp";
 import petryLogo from "../assets/petry.webp";
 import gyermelyiLogo from "../assets/gyermelyi.webp";
+import nastimedLogo from "../assets/nastimed-logo.png";
 import aicLogo from "../assets/aic.webp";
 import thereziaLogo from "../assets/therezia.webp";
 import demmersLogo from "../assets/demmer.webp";
@@ -40,6 +84,7 @@ import mmmLogo from "../assets/mmm.jpg";
 import kimLogo from "../assets/kim.jpg";
 import rakocziLogo from "../assets/rakoczi.png";
 import gecseLogo from "../assets/gecse.jpg";
+import { transformer } from "zod";
 
 const Sponsors = () => {
   return (
@@ -67,18 +112,6 @@ const Sponsors = () => {
             className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
           />
           <img
-            src={pmtgmLogo}
-            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
-          />
-          <img
-            src={cjmLogo}
-            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
-          />
-          <img
-            src={mmmLogo}
-            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
-          />
-          <img
             src={eetLogo}
             className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
           />
@@ -86,20 +119,46 @@ const Sponsors = () => {
             src={kimLogo}
             className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
           />
+
           <img
             src={ntpLogo}
             className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
           />
+          {/* <img
+            src={cjmLogo}
+            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+          /> */}
           <img
-            src={itmLogo}
-            className="h-32 w-32 object-scale-down transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+            src={sapientiaLogo}
+            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
           />
+          <img
+            src={pmtgmLogo}
+            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+          />
+          <img
+            src={mmtLogo}
+            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+          />
+          <img
+            src={mmmLogo}
+            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+          />
+
           <img
             src={emeLogo}
             className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
           />
           <img
-            src={sapientiaLogo}
+            src={spLogo}
+            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+          />
+          <img
+            src={megvalosultLogo}
+            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+          />
+          <img
+            src={npaLogo}
             className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
           />
           <img
@@ -107,21 +166,8 @@ const Sponsors = () => {
             className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
           />
           <img
-            src={bgaLogo}
-            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
-          />
-          <img
-            src={megvalosultLogo}
-            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
-          />
-
-          <img
-            src={spLogo}
-            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
-          />
-          <img
-            src={szbaLogo}
-            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+            src={itmLogo}
+            className="h-32 w-32 object-scale-down transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
           />
           <img
             src={rakocziLogo}
@@ -129,6 +175,22 @@ const Sponsors = () => {
           />
           <img
             src={gecseLogo}
+            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+          />
+          <img
+            src={pontisLogo}
+            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+          />
+          <img
+            src={szbaLogo}
+            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+          />
+          <img
+            src={cmrLogo}
+            className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+          />
+          <img
+            src={cfrLogo}
             className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
           />
           {/* <img
@@ -185,7 +247,31 @@ const Sponsors = () => {
       <h2 className="text-center text-5xl font-bold">Kiemelt Támogatóink</h2>
       <div className="flex flex-wrap justify-center gap-4 space-y-2 px-20">
         <img
+          src={toyotaLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={storzLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={tataiLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
           src={opLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={cardiomedLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={bioeelLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={clinilabLogo}
           className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
         />
         <img
@@ -193,11 +279,23 @@ const Sponsors = () => {
           className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
         />
         <img
+          src={gndLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={fomcoLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={labventLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
           src={bgLogo}
           className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
         />
         <img
-          src={endIboLogo}
+          src={ginemedLogo}
           className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
         />
       </div>
@@ -208,11 +306,32 @@ const Sponsors = () => {
           className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
         />
         <img
+          src={spLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={medicinaLogo}
+          className="mix-blend-color-multiply h-32 w-64 object-scale-down transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
           src={gym18Logo}
           className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
         />
         <img
+          src={angioLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={tablourLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+
+        <img
           src={pcLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={aomLogo}
           className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
         />
         <img
@@ -220,7 +339,47 @@ const Sponsors = () => {
           className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
         />
         <img
+          src={marmedLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={nastimedLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={osteoLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={zpLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={beatLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
           src={endIboLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={pavaLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={dwLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={countryLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={dcLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={vitalityLogo}
           className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
         />
         <img
@@ -240,7 +399,71 @@ const Sponsors = () => {
           className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
         />
         <img
+          src={biaLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={hookahLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={crLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={invitedorLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={bsbLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
           src={gordonLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={hcLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={jockerLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={lcLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={naturaliLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={daliaLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={ponpaonLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={dscLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={bertisLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={tobozLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={ikLogo}
+          className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
+        />
+        <img
+          src={darociLogo}
           className="h-32 w-64 object-scale-down mix-blend-multiply transition-transform duration-150 ease-linear hover:scale-105 md:h-48 lg:w-96"
         />
       </div>
