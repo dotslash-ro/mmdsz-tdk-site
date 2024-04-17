f1ccef7e89f728430b492d466fc22a915c07be3b
godraadam <godra.adam@gmail.com>
2024-04-17 19:55:41 +0300

    enable workshop signup

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -114,7 +114,7 @@ const Navbar = () => {
               </li>
               <li>
                 <NavLink
-                  to="/workshop"
+                  to="/workshop-jelentkezes"
                   className={({ isActive }) =>
                     isActive
                       ? "lg:font-base flex items-center justify-center py-3 text-sm font-bold uppercase text-white underline lg:py-0"
